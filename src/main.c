ab4631010da64c258775c4c985279a923521187f
Alex Fihl Hedegaard Nielsen <Alex@fihl.net>
2018-06-08 17:02:31 +0200

    added simon fuction

--- src/main.c
+++ src/main.c
@@ -23,7 +23,66 @@ extern uint8_t lcdBuffer[512];
 uint8_t updateGame;
 uint8_t gameSpeed;
 
-
+void simon()
+{
+    struct wall_t wall;
+    struct vector_t v1, v2, v3, v4;
+    uint16_t i, x, y;
+    intVector(&v1, 3, 1);
+    intVector(&v2, 218, 63);
+    intWall(&wall, &v1, &v2);
+    drawWall(&wall);
+    struct block_t* blocks = malloc(100 * sizeof *blocks);
+    intVector(&v3, 10, 10);
+    intVector(&v4, 45, 45);
+    x = 4;
+    y = 12;
+    intMultipleBlocks(&blocks, v3, v4, x, y);
+    for (i = 0; i < x * y; i++)
+        drawBlock(&blocks[i]);
+    struct striker_t striker1;
+    intStriker(&striker1);
+    drawStriker(&striker1);
+    struct ball_t b;
+    intBall(&b, 110, 62, -5, -5);
+    drawBall(&b);
+    uint8_t k = 1;
+    while(1){
+        if (updateGame > 0){
+            updatePosition(&b, &wall, &blocks, x * y);
+            drawBall(&b);
+            switch (k)
+            {
+            case(1):
+                changeStrikerLength(&striker1,-4);
+                drawStriker(&striker1);
+                break;
+            case(2):
+                updateStriker(&striker1,-10);
+                drawStriker(&striker1);
+                break;
+            case(3):
+                changeStrikerLength(&striker1,4);
+                drawStriker(&striker1);
+                break;
+            case(4):
+                updateStriker(&striker1,10);
+                drawStriker(&striker1);
+                break;
+            case(5):
+                k = 0;
+                break;
+            default:
+                break;
+            }
+            k++;
+            for (i = 0; i < x*y; i++)
+                drawBlock(&blocks[i]);
+            updateGame = 0;
+            setLed(1,1,0);
+        }
+    }
+}
 
 void alex()
 {
