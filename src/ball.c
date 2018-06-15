cdb47e0e32e788dabab1dd87caa157e7c4b199df
Alex Fihl Hedegaard Nielsen <Alex@fihl.net>
2018-06-15 14:56:37 +0200

    turn the sound back on

--- src/ball.c
+++ src/ball.c
@@ -88,7 +88,7 @@ void updatePosition(struct ball_t *b, struct wall_t *w, struct block_t ** blocks
         {
             if (newX >= block.v1.x && newX <= (block.v2.x + (1 << 14)) && newY >= block.v1.y && newY <= (block.v2.y + (1 << 14)))
             {
-                //setFreq(1000);
+                setFreq(1000);
                 if(block.v1.x >= b->oldPos.x || (block.v2.x + (1 << 14)) <= b->oldPos.x)
                 {
                     b->angle =  - b->angle;
@@ -121,7 +121,7 @@ void updatePosition(struct ball_t *b, struct wall_t *w, struct block_t ** blocks
     int32_t lenghtHalf = (s->length) >> 1;
     if ((newX >= s->center.x - lenghtHalf) && (newX <= s->center.x + lenghtHalf) && (newY >= s->center.y - 0x00003000)) //0x00003000 = 0.750
     {
-        //setFreq(10000);
+        setFreq(10000);
         int32_t y=s->length;
         b->angle =  256 - b->angle + (((newX - s->center.x) * 60 )/(y >> 1));
         while (b->angle > 256 && b->angle < -256)
