049f78ceeb9088696a73c69e6a2fe1c2a3ce1502
Alex Fihl Hedegaard Nielsen <Alex@fihl.net>
2018-06-20 12:32:20 +0200

    #71  for gameWall

--- inc/gameWall.h
+++ inc/gameWall.h
@@ -14,8 +14,6 @@ void drawWall(struct wall_t *w);
 //To be able to draw a window with a title
 
 void window  (struct wall_t *w, char text[], uint8_t style);
-void windowFG(struct wall_t *w, char text[], uint8_t style, uint8_t colorText, uint8_t colorBond);
-void windowBG(struct wall_t *w, char text[], uint8_t style, uint8_t colorText, uint8_t colorTextbg, uint8_t colorBond, uint8_t colorBondbg);
 
 
 #endif //GAMEWALL_H
